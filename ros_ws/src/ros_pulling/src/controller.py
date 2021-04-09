684903b7c0adff4c82dc75232faaaa20753b4758
tiffanyec <tiffanyec@berkeley.edu>
2021-04-08 21:42:24 -0700

    just car p controller plots vel and pos

--- ros_ws/src/ros_pulling/src/controller.py
+++ ros_ws/src/ros_pulling/src/controller.py
@@ -16,14 +16,15 @@ from numpy.linalg import inv
 class Controller():
 
     def __init__(self):
-        self.K = 10.0
+        self.K = 20.0
         self.all_curr_vel = []
         self.time = []
+        self.all_pos = []
         self.vel_pub = rospy.Publisher('/vrep_ros_interface/car_vel', Float32MultiArray, queue_size=10)
         rospy.Subscriber('/vrep_ros_interface/car_curr_vel', Float32MultiArray, self.vel_callback, queue_size=1, buff_size=2**8)
+        rospy.Subscriber('/vrep_ros_interface/car_pos', Float32MultiArray, self.pos_callback, queue_size=1, buff_size=2**8)
         rospy.spin()
         self.plotting()
-
 
     def vel_callback(self, msg):
         print('hello from vel_callback')
@@ -36,16 +37,29 @@ class Controller():
         vel.data = [msg.data[0], self.K*error_y, msg.data[2]]
         self.vel_pub.publish(vel)
     
+    def pos_callback(self, msg):
+        self.all_pos.append(msg.data[1])
+    
     def plotting(self):
         # print("hello from plotting")
-        print(self.all_curr_vel)
+        # print(self.all_curr_vel)
         fig, ax = plt.subplots()  # Create a figure and an axes.
         ax.plot(self.time, np.array(self.all_curr_vel)[:,0], color='tab:pink', label='x vel')  # Plot some data on the axes.
         ax.plot(self.time, np.array(self.all_curr_vel)[:,1], color='tab:cyan', label='y vel')  # Plot more data on the axes...
         plt.legend()
-        ax.set_xlabel('Time')  # Add an x-label to the axes.
+        ax.set_xlabel('Time (s)')  # Add an x-label to the axes.
         color = 'tab:red'
-        ax.set_ylabel('Velocity', color=color)
+        ax.set_ylabel('Velocity (m/s)', color=color)
+        ax.set_ylim([-0.1, 0.1])
+
+        ax2 = ax.twinx()
+        ax2.plot(self.time, self.all_pos[:len(self.time)], label='car y position')
+        color = 'tab:blue'
+        ax2.set_ylabel('y-axis Position (m)', color=color)
+        ax2.set_ylim([-0.5, 0])
+
+        plt.title('Car x, y Velocity and y Position with P Controller')
+
         plt.legend()
         plt.show()
 
