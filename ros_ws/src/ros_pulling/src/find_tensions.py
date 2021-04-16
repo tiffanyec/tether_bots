f7581eff07a27f322c99e09f59fb690681e8f3ff
tiffanyec <tiffanyec@berkeley.edu>
2021-04-15 20:07:48 -0700

    update find_tensions to take in desired angle of block and can now call robo_block controller

--- ros_ws/src/ros_pulling/src/find_tensions.py
+++ ros_ws/src/ros_pulling/src/find_tensions.py
@@ -12,10 +12,12 @@ import scipy
 from scipy import signal
 from mpl_toolkits.mplot3d import Axes3D
 from numpy.linalg import inv
+import robo_block
 
 class Tensions():
 
-    def __init__(self):
+    def __init__(self, des_angle):
+        self.des_angle = des_angle
         self.left_avg_tension = []
         self.right_avg_tension = []
         self.ten_time = []
@@ -24,6 +26,7 @@ class Tensions():
         self.r = 0.3/2
         self.left_dist = 0
         self.M = 0.2
+        self.I = (1/3)*(self.M)*((2*self.r)^2 + (2*self.d)^2)
         # self.right_dist = 0
         self.car1_pos = []
         self.car2_pos = []
@@ -39,13 +42,17 @@ class Tensions():
         self.left_car_pos = []
         self.right_car_pos = []
         self.block_all_pos = []
+        self.robo_block1 = robo_block(None, 0.05, 1, None, None)
+        self.robo_block2 = robo_block(None, 0.05, 2, None, None)
         rospy.Subscriber('/vrep_ros_interface/block_angle', Float32MultiArray, self.block_angle_callback, queue_size=1, buff_size=2**8)
         rospy.Subscriber('/vrep_ros_interface/block_pos', Float32MultiArray, self.pos2_callback, queue_size = 1, buff_size=2**8)
         rospy.Subscriber('/vrep_ros_interface/car1_pos', Float32MultiArray, self.pos1_callback, queue_size = 1, buff_size=2**8)
         rospy.Subscriber('/vrep_ros_interface/car2_pos', Float32MultiArray, self.pos3_callback, queue_size = 1, buff_size=2**8)
         rospy.Subscriber('/vrep_ros_interface/tensions', Float32MultiArray, self.ten_callback, queue_size = 1, buff_size=2**8)
 
-        rospy.spin()
+        while not rospy.is_shutdown():
+            self.control()
+
         self.plotting()
 
 
@@ -82,34 +89,82 @@ class Tensions():
     def car1_find_angle(self):
         c = ((self.d*2)**2 + (self.rope1_len)**2 - (self.left_dist)**2) / (2*(self.d*2)*(self.rope1_len))
         self.car1_angle = np.arccos(c)
-        self.calculate()
+        # self.control()
     
     def car2_find_angle(self):
         g = np.arcsin(self.block_pos[2] / (self.diag + self.rope2_len))
         self.car2_angle = 90 - g
-        self.calculate()
+        # self.control()
 
-
-    def calculate(self):
+    # control scheme: find desired wrench to apply to block and calculate necessary tensions to do so and then call robo_block.py for each car to apply the tensions
+    def control(self):
         if self.car1_angle != 0 and self.car2_angle != 0 and self.block_angle != 0:
             theta1 = self.car1_angle
             theta2 = self.car2_angle
-            theta3 = self.block_angle
-            d = self.d
-            r = self.r
-            ten_mat = np.array([[np.sin(theta1), np.sin(theta2), -np.sin(theta3)],
-                                [-np.cos(theta1), -np.cos(theta2), np.cos(theta3)],
-                                [-d*np.sin(theta1) + r*np.cos(theta1), d*np.sin(theta2) - r*np.cos(theta2), -r*np.cos(theta3) - d*np.sin(theta3)]])
-            des = np.array([[0], 
-                            [0], 
-                            [self.M*9.81*d*np.cos(theta3)]])
-            ten_mat_inv = inv(ten_mat)
-            out = ten_mat_inv.dot(des)
-            self.des_left_ten.append(out[0][0])
-            self.des_right_ten.append(out[1][0])
-            print("tension1: " + str(out[0]))
-            print("tension2: " + str(out[1]))
-            print("f: " + str(out[2]))
+            theta3 = self.des_angle
+            torque = self.M*9.81*d*np.cos(self.block_angle)
+            error = theta3 - self.block_angle
+            T = 7 # time for acceleration
+
+            self.robo_block1.curr_tension = self.left_avg_tension
+            self.robo_block1.curr_pos = self.left_car_pos[-1]
+
+            self.robo_block2.curr_tension = self.right_avg_tension
+            self.robo_block2.curr_pos = self.right_car_pos[-1]
+
+            # want torque greater than gravity to rotate block (positive angular acceleration)
+            if error > 0.01:
+                des_ang_acc = (4* error) / (T^2)
+                torque += (self.I*des_ang_acc) # adding torque to rotate block
+                des_wrench = np.array([[0], 
+                                [0], 
+                                [torque]])
+                out = self.calculate(theta1, theta2, theta3, des_wrench)
+                self.des_left_ten.append(out[0][0])
+                self.des_right_ten.append(out[1][0])
+
+                # set des_t for robo_block for car1 and car2
+                self.robo_block1.des_t = out[0][0]
+                self.robo_block2.des_t = out[1][0]
+
+
+            # just resist gravity and stop rotating if within error tolerance
+            else:
+                des_wrench = np.array([[0], 
+                                [0], 
+                                [torque]])
+                out = self.calculate(theta1, theta2, theta3, des_wrench)
+                self.des_left_ten.append(out[0][0])
+                self.des_right_ten.append(out[1][0])
+
+                # set des_t for robo_block for car1 and car2
+                self.robo_block1.des_t = out[0][0]
+                self.robo_block2.des_t = out[1][0]
+            
+            # call robo_block control
+            while not self.robo_block1.done and not self.robo_block2.done:
+                self.robo_block1.curr_tension = self.left_avg_tension
+                self.robo_block1.curr_pos = self.left_car_pos[-1]
+
+                self.robo_block2.curr_tension = self.right_avg_tension
+                self.robo_block2.curr_pos = self.right_car_pos[-1]
+                
+                self.robo_block1.control()
+                self.robo_block2.control()
+
+                
+
+    
+    # calculate necessary tensions
+    def calculate(self, theta1, theta2, theta3, des_wrench):
+        d = self.d
+        r = self.r
+        ten_mat = np.array([[np.sin(theta1), np.sin(theta2), -np.sin(theta3)],
+                            [-np.cos(theta1), -np.cos(theta2), np.cos(theta3)],
+                            [-d*np.sin(theta1) + r*np.cos(theta1), d*np.sin(theta2) - r*np.cos(theta2), -r*np.cos(theta3) - d*np.sin(theta3)]])
+        ten_mat_inv = inv(ten_mat)
+        out = ten_mat_inv.dot(des_wrench)
+        return out
 
     
     def plotting(self):
--- ros_ws/src/ros_pulling/src/robo_block.py
+++ ros_ws/src/ros_pulling/src/robo_block.py
@@ -15,39 +15,54 @@ from numpy.linalg import inv
 
 class Robo_Block():
 
-    def __init__(self, des_t, pos_inc, car=1):
+    def __init__(self, des_t, pos_inc, car=1, curr_tension, curr_pos):
+        # rospy.init_node("robo_block", anonymous=True)
         self.des_t = des_t
         self.pos_inc = pos_inc
-        self.des_pos = null
-        self.avg_tension = []
-        self.ten_time = []
-        self.all_pos = []
+        self.des_pos = None
+        self.car = car
+        self.curr_tension = curr_tension
+        # self.ten_time = []
+        self.curr_pos = curr_pos
         self.done = False
-        rospy.Subscriber('/vrep_ros_interface/tensions', Float32MultiArray, self.ten_callback, queue_size = 1, buff_size=2**8)
-        rospy.Subscriber('/vrep_ros_interface/car_pos' + str(car), Float32MultiArray, self.pos1_callback, queue_size=1, buff_size=2**8)
-
-        rospy.spin()
-
-    def ten_callback(self, msg):
-        print('time: ' + str(msg.data[4]))
-
-        self.left_avg_tension.append(np.abs((msg.data[0] + msg.data[1]) / 2))
-        self.right_avg_tension.append(np.abs((msg.data[2] + msg.data[3]) / 2))
-        self.ten_time.append(msg.data[4])
-
-    # car
-    def pos1_callback(self, msg):
-        self.all_pos.append(msg.data)
-        x = msg.data[0]
+        self.error = None
+        x = self.curr_pos[0]
         if x < 0:
             self.des_pos = x - self.pos_inc
         else:
             self.des_pos = x + self.pos_inc
+        # rospy.Subscriber('/vrep_ros_interface/tensions', Float32MultiArray, self.ten_callback, queue_size = 1, buff_size=2**8)
+        # rospy.Subscriber('/vrep_ros_interface/car_pos' + str(car), Float32MultiArray, self.pos1_callback, queue_size=1, buff_size=2**8)
+
+        # while not self.done:
+        #     self.control()
+        # rospy.spin()
+
+    # def ten_callback(self, msg):
+    #     # print('time: ' + str(msg.data[4]))
+    #     if self.car == 1:
+    #         self.avg_tension.append(np.abs((msg.data[0] + msg.data[1]) / 2))
+    #     else:
+    #         self.avg_tension.append(np.abs((msg.data[2] + msg.data[3]) / 2))
+    #     self.ten_time.append(msg.data[4])
+
+    #     # self.control()
+
+    # # car
+    # def pos1_callback(self, msg):
+    #     self.all_pos.append(msg.data)
+    #     x = msg.data[0]
+    #     if x < 0:
+    #         self.des_pos = x - self.pos_inc
+    #     else:
+    #         self.des_pos = x + self.pos_inc
     
     def control(self):
-        error = self.des_t - self.avg_tension[-1]
-        if error > 0.01:
-            # call car controller here
-        else:
-            self.done = True
+        if self.avg_tension != [] and self.des_pos:
+            error = self.des_t - self.avg_tension[-1]
+            if error > 0.01:
+                # TODO call car controller here
+            else:
+                # TODO call car controller to stop
+                self.done = True
 
